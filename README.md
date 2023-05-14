c4c772d86158d030b799d750b9ec8f64cd388892
秦亚东 <Night.Qin@outlook.com>
2023-05-14 22:46:32 +0800

    [优化]描述文件说明

--- README.md
+++ README.md
@@ -6,9 +6,7 @@
 基于原生Dialog封装，自带仿鸿蒙主题Dialog
 
 ## 2.安装教程
-
 在`setting.gradle`文件中加入如下代码
-
 ```
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -23,72 +21,60 @@ dependencyResolutionManagement {
 
 ```
 dependencies {
-	implementation 'com.github.NightRainDream:EaseDialog:Tag'
+	implementation 'com.github.NightRainDream:EaseDialog:VERISON'
 }
 ```
 ## 3.使用说明
-
 ### 3.1.初始化Module
-
 在Application初始化
-
-```
+```kotlin
 override fun onCreate() {
    super.onCreate()
    EaseDialog.initialize(this)
 }
 ```
-### 3.2.自带样式
-
+### 3.2.普通对话框
 #### 3.2.1.提示性质对话框
-
 ```kotlin
 DialogTools.getDialogBuilder()
     .setTitleText("允许日历联网")
     .setMainText("下载或更新节假日、班休、历法、订阅等信息。")
     .setPositiveTextColor(Color.RED)
-    .toTipsDialog(this, object : IDialogActionCallback {
-        override fun onPositive(content: String, index: MutableList<Int>) {
-            DialogTools.getToastBuilder()
-                .toToast("确定")
+    .toTipsDialog(this, object : ITipsModalCallback {
+        override fun onPositive() {
+            DialogTools.getToastBuilder().toToast("点击确定")
         }
 
         override fun onCancel() {
-            DialogTools.getToastBuilder()
-                .toToast("取消")
+            DialogTools.getToastBuilder().toToast("点击取消")
         }
     })
 ```
 
 #### 3.2.2.警告性质对话框
-
 ```kotlin
 DialogTools.getDialogBuilder()
-    .setMainText("确定删除当前联系人确定删除当前联系人确定删除当前联系人?")
+    .setMainText("确定删除当前联系人?")
     .setPositiveTextColor(Color.RED)
     .setPositiveText("删除")
-    .toWarnDialog(this, object : IDialogActionCallback {
-        override fun onPositive(content: String, index: MutableList<Int>) {
-            DialogTools.getToastBuilder()
-                .toToast("确定")
+    .toWarnDialog(this, object : IWarnModalCallback {
+        override fun onPositive() {
+            DialogTools.getToastBuilder().toToast("点击确定")
         }
 
         override fun onCancel() {
-            DialogTools.getToastBuilder()
-                .toToast("取消")
+            DialogTools.getToastBuilder().toToast("点击取消")
         }
     })
 ```
 
 #### 3.2.3.加载对话框
-
 ```kotlin
 DialogTools.getDialogBuilder()
     .toLoadingDialog(this)
 ```
-
-#### 3.2.4.单选菜单
-
+### 3.3.选择菜单
+#### 3.3.1.底部单选菜单
 ```kotlin
 DialogTools.getDialogBuilder()
     .setTitleText("重复")
@@ -96,50 +82,55 @@ DialogTools.getDialogBuilder()
         this,
         mSingleList,
         mSelectSingleIndex,
-        object : IDialogActionCallback {
-            override fun onPositive(content: String, index: MutableList<Int>) {
-                mSelectSingleIndex = index.first()
-                DialogTools.getToastBuilder()
-                    .toToast(content)
+        object : ISingleMenuCallback {
+            override fun onPositive(title: String, menuIndex: Int) {
+                mSelectSingleIndex = menuIndex
+                DialogTools.getToastBuilder().toToast("选中内容:$title")
             }
 
             override fun onCancel() {
-                super.onCancel()
-                DialogTools.getToastBuilder()
-                    .toToast("取消")
+                DialogTools.getToastBuilder().toToast("点击取消")
             }
         })
 ```
-
-#### 3.2.5.多选菜单
-
+#### 3.3.2.底部多选菜单
 ```kotlin
 DialogTools.getDialogBuilder()
     .setTitleText("重复")
     .toMultipleMenu(this, mMultipleList, mSelectMultipleIndex,
-        object : IDialogActionCallback {
-            override fun onPositive(content: String, index: MutableList<Int>) {
+        object : IMultipleMenuCallback {
+            override fun onPositive(title: List<String>, menuIndex: List<Int>) {
                 mSelectMultipleIndex.clear()
-                mSelectMultipleIndex.addAll(index)
-                DialogTools.getToastBuilder()
-                    .toToast("确定==>" + index.size)
+                mSelectMultipleIndex.addAll(menuIndex)
+                DialogTools.getToastBuilder().toToast("选中内容:${menuIndex.size}个")
             }
 
             override fun onCancel() {
-                super.onCancel()
-                DialogTools.getToastBuilder()
-                    .toToast("取消")
+                DialogTools.getToastBuilder().toToast("点击取消")
             }
         })
 ```
+#### 3.3.3.Pop菜单
+```kotlin
+DialogTools.getDialogBuilder()
+    .setTouchCoordinate(event.x, event.y, event.rawX, event.rawY)
+    .toPopMenu(this, v, mutableListOf("删除", "编辑", "复制"), object : IPopMenuCallback {
+        override fun onPositive(title: String, menuIndex: Int) {
+            DialogTools.getToastBuilder().toToast(title)
+        }
+    })
+```
 
-#### 3.2.6.日期时间选择器
-
+### 3.4.选择器
+#### 3.4.1.日期时间选择器
 ```kotlin
 DialogTools.getPickerBuilder()
     .setTitleText("请选择日期和时间")
-    .toDateTimePicker(this,PICKER_ALL, object : IDateTimeCallback {
-        override fun onSelectDate(result: EaseDateTimeEntity) {
+    .setMinDateTime(mStartDateTime)
+    .setMaxDateTime(mEndDateTime)
+    .setSelDateTime(mSelectDateTime)
+    .toDateTimePicker(this, PICKER_ALL, object : IDateTimeCallback {
+        override fun onPositive(result: EaseDateTimeEntity) {
             DialogTools.getToastBuilder()
                 .toToast(result.getDateTime())
         }
@@ -152,7 +143,7 @@ DialogTools.getPickerBuilder()
     })
 ```
 
-#### 3.2.7.地址选择器
+#### 3.4.2.地址选择器
 ```kotlin
 DialogTools.getPickerBuilder()
     .setTitleText("请选择地址")
@@ -160,7 +151,7 @@ DialogTools.getPickerBuilder()
     .setSelectCity(mCity)
     .setSelectCounty(mCounty)
     .toAddressPicker(this, PICKER_ADDRESS_PROVINCE_CITY, object : ILocationCallback {
-        override fun onAddressSelected(
+        override fun onPositive(
             province: EaseLocationEntity,
             city: EaseLocationEntity,
             county: EaseLocationEntity
@@ -172,18 +163,19 @@ DialogTools.getPickerBuilder()
         }
     })
 ```
-#### 3.2.8.颜色选择器
+#### 3.4.2.颜色选择器
 ```kotlin
 DialogTools.getPickerBuilder()
     .setTitleText("请选择颜色")
     .toColorPicker(this, object : IColorSelectCallback {
-        override fun onSelected(red: Int, green: Int, blue: Int) {
-            DialogTools.getToastBuilder().toToast("R:".plus(red).plus("|G:").plus(green).plus("|B:").plus(blue))
+        override fun onPositive(red: Int, green: Int, blue: Int) {
+            DialogTools.getToastBuilder()
+                .toToast("R:".plus(red).plus("|G:").plus(green).plus("|B:").plus(blue))
         }
     })
 ```
-
-#### 3.2.9.Toast
+### 3.5.其他
+#### 3.5.1.Toast
 ```kotlin
 DialogTools.getToastBuilder()
     .toToast("文件删除成功~")
--- app/src/main/java/com/night/easedialog/MainActivity.kt
+++ app/src/main/java/com/night/easedialog/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.night.easedialog
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import com.night.dialog.DialogTools
@@ -41,161 +41,163 @@ class MainActivity : AppCompatActivity() {
         "周日", "周一", "周二", "周三", "周四", "周五", "周六", "黑色星期一"
     )
     private val mSelectMultipleIndex = mutableListOf<Int>()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mTipsView = findViewById(R.id.btn_tips_dialog)
-        mWarnView = findViewById(R.id.btn_warn_dialog)
-        mSingleView = findViewById(R.id.btn_single_dialog)
-        mMultipleView = findViewById(R.id.btn_multiple_dialog)
-        mSingleMenuleView = findViewById(R.id.btn_single_menu)
-        mDateTimePickerleView = findViewById(R.id.btn_date_time_picker)
-        mAddressPickerleView = findViewById(R.id.btn_address_picker)
-        mColorPickerleView = findViewById(R.id.btn_color_picker)
-        mLoadingView = findViewById(R.id.btn_loading)
-        mToastView = findViewById(R.id.btn_toast)
+    @SuppressLint("ClickableViewAccessibility")
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_main)
+mTipsView = findViewById(R.id.btn_tips_dialog)
+mWarnView = findViewById(R.id.btn_warn_dialog)
+mSingleView = findViewById(R.id.btn_single_dialog)
+mMultipleView = findViewById(R.id.btn_multiple_dialog)
+mSingleMenuleView = findViewById(R.id.btn_single_menu)
+mDateTimePickerleView = findViewById(R.id.btn_date_time_picker)
+mAddressPickerleView = findViewById(R.id.btn_address_picker)
+mColorPickerleView = findViewById(R.id.btn_color_picker)
+mLoadingView = findViewById(R.id.btn_loading)
+mToastView = findViewById(R.id.btn_toast)
 
-        //提示性质模态框
-        mTipsView.setOnClickListener {
-            DialogTools.getDialogBuilder()
-                .setTitleText("允许日历联网")
-                .setMainText("下载或更新节假日、班休、历法、订阅等信息。")
-                .setPositiveTextColor(Color.RED)
-                .toTipsDialog(this, object : ITipsModalCallback {
-                    override fun onPositive() {
-                        DialogTools.getToastBuilder().toToast("点击确定")
-                    }
+//提示性质模态框
+mTipsView.setOnClickListener {
+DialogTools.getDialogBuilder()
+    .setTitleText("允许日历联网")
+    .setMainText("下载或更新节假日、班休、历法、订阅等信息。")
+    .setPositiveTextColor(Color.RED)
+    .toTipsDialog(this, object : ITipsModalCallback {
+        override fun onPositive() {
+            DialogTools.getToastBuilder().toToast("点击确定")
+        }
 
-                    override fun onCancel() {
-                        DialogTools.getToastBuilder().toToast("点击取消")
-                    }
-                })
+        override fun onCancel() {
+            DialogTools.getToastBuilder().toToast("点击取消")
         }
-        //警告性质模态框
-        mWarnView.setOnClickListener {
-            DialogTools.getDialogBuilder()
-                .setMainText("确定删除当前联系人?")
-                .setPositiveTextColor(Color.RED)
-                .setPositiveText("删除")
-                .toWarnDialog(this, object : IWarnModalCallback {
-                    override fun onPositive() {
-                        DialogTools.getToastBuilder().toToast("点击确定")
-                    }
+    })
+}
+//警告性质模态框
+mWarnView.setOnClickListener {
+DialogTools.getDialogBuilder()
+    .setMainText("确定删除当前联系人?")
+    .setPositiveTextColor(Color.RED)
+    .setPositiveText("删除")
+    .toWarnDialog(this, object : IWarnModalCallback {
+        override fun onPositive() {
+            DialogTools.getToastBuilder().toToast("点击确定")
+        }
 
-                    override fun onCancel() {
-                        DialogTools.getToastBuilder().toToast("点击取消")
-                    }
-                })
+        override fun onCancel() {
+            DialogTools.getToastBuilder().toToast("点击取消")
         }
-        //底部单选菜单
-        mSingleView.setOnClickListener {
-            DialogTools.getDialogBuilder()
-                .setTitleText("重复")
-                .toSingleMenu(
-                    this,
-                    mSingleList,
-                    mSelectSingleIndex,
-                    object : ISingleMenuCallback {
-                        override fun onPositive(title: String, menuIndex: Int) {
-                            mSelectSingleIndex = menuIndex
-                            DialogTools.getToastBuilder().toToast("选中内容:$title")
-                        }
-
-                        override fun onCancel() {
-                            DialogTools.getToastBuilder().toToast("点击取消")
-                        }
-                    })
-        }
-        //底部多选菜单
-        mMultipleView.setOnClickListener {
-            DialogTools.getDialogBuilder()
-                .setTitleText("重复")
-                .toMultipleMenu(this, mMultipleList, mSelectMultipleIndex,
-                    object : IMultipleMenuCallback {
-                        override fun onPositive(title: List<String>, menuIndex: List<Int>) {
-                            mSelectMultipleIndex.clear()
-                            mSelectMultipleIndex.addAll(menuIndex)
-                            DialogTools.getToastBuilder().toToast("选中内容:${menuIndex.size}个")
-                        }
-
-                        override fun onCancel() {
-                            DialogTools.getToastBuilder().toToast("点击取消")
-                        }
-                    })
-        }
-        //时间日期选择器
-        mDateTimePickerleView.setOnClickListener {
-            DialogTools.getPickerBuilder()
-                .setTitleText("请选择日期和时间")
-                .setMinDateTime(mStartDateTime)
-                .setMaxDateTime(mEndDateTime)
-                .setSelDateTime(mSelectDateTime)
-                .toDateTimePicker(this, PICKER_ALL, object : IDateTimeCallback {
-                    override fun onPositive(result: EaseDateTimeEntity) {
-                        DialogTools.getToastBuilder()
-                            .toToast(result.getDateTime())
-                    }
-
-                    override fun onCancel() {
-                        super.onCancel()
-                        DialogTools.getToastBuilder()
-                            .toToast("取消")
-                    }
-                })
-        }
-        //地址选择器
-        mAddressPickerleView.setOnClickListener {
-            DialogTools.getPickerBuilder()
-                .setTitleText("请选择地址")
-                .setSelectProvince(mProvince)
-                .setSelectCity(mCity)
-                .setSelectCounty(mCounty)
-                .toAddressPicker(this, PICKER_ADDRESS_PROVINCE_CITY, object : ILocationCallback {
-                    override fun onPositive(
-                        province: EaseLocationEntity,
-                        city: EaseLocationEntity,
-                        county: EaseLocationEntity
-                    ) {
-                        mProvince = province
-                        mCity = city
-                        mCounty = county
-                        DialogTools.getToastBuilder().toToast(province.name + city.name + county.name)
-                    }
-                })
-        }
-        //颜色选择器
-        mColorPickerleView.setOnClickListener {
-            DialogTools.getPickerBuilder()
-                .setTitleText("请选择颜色")
-                .toColorPicker(this, object : IColorSelectCallback {
-                    override fun onPositive(red: Int, green: Int, blue: Int) {
-                        DialogTools.getToastBuilder()
-                            .toToast("R:".plus(red).plus("|G:").plus(green).plus("|B:").plus(blue))
-                    }
-                })
-        }
-        //加载框
-        mLoadingView.setOnClickListener {
-            DialogTools.getDialogBuilder()
-                .toLoadingDialog(this, "正在加载中...")
-        }
-        //Toast
-        mToastView.setOnClickListener {
-            DialogTools.getToastBuilder()
-                .toToast("文件删除成功~")
-        }
-        //单选菜单
-        mSingleMenuleView.setOnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                DialogTools.getDialogBuilder()
-                    .setTouchCoordinate(event.x, event.y, event.rawX, event.rawY)
-                    .toPopMenu(this, v, mutableListOf("删除", "编辑", "复制"), object : IPopMenuCallback {
-                        override fun onPositive(title: String, menuIndex: Int) {
-                            DialogTools.getToastBuilder().toToast(title)
-                        }
-                    })
+    })
+}
+//底部单选菜单
+mSingleView.setOnClickListener {
+DialogTools.getDialogBuilder()
+    .setTitleText("重复")
+    .toSingleMenu(
+        this,
+        mSingleList,
+        mSelectSingleIndex,
+        object : ISingleMenuCallback {
+            override fun onPositive(title: String, menuIndex: Int) {
+                mSelectSingleIndex = menuIndex
+                DialogTools.getToastBuilder().toToast("选中内容:$title")
             }
-            true
+
+            override fun onCancel() {
+                DialogTools.getToastBuilder().toToast("点击取消")
+            }
+        })
+}
+//底部多选菜单
+mMultipleView.setOnClickListener {
+DialogTools.getDialogBuilder()
+    .setTitleText("重复")
+    .toMultipleMenu(this, mMultipleList, mSelectMultipleIndex,
+        object : IMultipleMenuCallback {
+            override fun onPositive(title: List<String>, menuIndex: List<Int>) {
+                mSelectMultipleIndex.clear()
+                mSelectMultipleIndex.addAll(menuIndex)
+                DialogTools.getToastBuilder().toToast("选中内容:${menuIndex.size}个")
+            }
+
+            override fun onCancel() {
+                DialogTools.getToastBuilder().toToast("点击取消")
+            }
+        })
+}
+//时间日期选择器
+mDateTimePickerleView.setOnClickListener {
+DialogTools.getPickerBuilder()
+    .setTitleText("请选择日期和时间")
+    .setMinDateTime(mStartDateTime)
+    .setMaxDateTime(mEndDateTime)
+    .setSelDateTime(mSelectDateTime)
+    .toDateTimePicker(this, PICKER_ALL, object : IDateTimeCallback {
+        override fun onPositive(result: EaseDateTimeEntity) {
+            DialogTools.getToastBuilder()
+                .toToast(result.getDateTime())
         }
-    }
+
+        override fun onCancel() {
+            super.onCancel()
+            DialogTools.getToastBuilder()
+                .toToast("取消")
+        }
+    })
+}
+//地址选择器
+mAddressPickerleView.setOnClickListener {
+DialogTools.getPickerBuilder()
+    .setTitleText("请选择地址")
+    .setSelectProvince(mProvince)
+    .setSelectCity(mCity)
+    .setSelectCounty(mCounty)
+    .toAddressPicker(this, PICKER_ADDRESS_PROVINCE_CITY, object : ILocationCallback {
+        override fun onPositive(
+            province: EaseLocationEntity,
+            city: EaseLocationEntity,
+            county: EaseLocationEntity
+        ) {
+            mProvince = province
+            mCity = city
+            mCounty = county
+            DialogTools.getToastBuilder().toToast(province.name + city.name + county.name)
+        }
+    })
+}
+//颜色选择器
+mColorPickerleView.setOnClickListener {
+DialogTools.getPickerBuilder()
+    .setTitleText("请选择颜色")
+    .toColorPicker(this, object : IColorSelectCallback {
+        override fun onPositive(red: Int, green: Int, blue: Int) {
+            DialogTools.getToastBuilder()
+                .toToast("R:".plus(red).plus("|G:").plus(green).plus("|B:").plus(blue))
+        }
+    })
+}
+//加载框
+mLoadingView.setOnClickListener {
+    DialogTools.getDialogBuilder()
+        .toLoadingDialog(this, "正在加载中...")
+}
+//Toast
+mToastView.setOnClickListener {
+    DialogTools.getToastBuilder()
+        .toToast("文件删除成功~")
+}
+//单选菜单
+
+mSingleMenuleView.setOnTouchListener { v, event ->
+if (event.action == MotionEvent.ACTION_UP) {
+DialogTools.getDialogBuilder()
+    .setTouchCoordinate(event.x, event.y, event.rawX, event.rawY)
+    .toPopMenu(this, v, mutableListOf("删除", "编辑", "复制"), object : IPopMenuCallback {
+        override fun onPositive(title: String, menuIndex: Int) {
+            DialogTools.getToastBuilder().toToast(title)
+        }
+    })
+}
+true
+}
+}
 }
